e04a2b9115f5fcb2376b9759ba1c44fc48a825a1
Anand <anandmohanjha241@gmail.com>
2024-07-21 23:39:13 +0530

    added videos and other things

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Highlights from './components/Highlights'
+import Hero from './components/Hero'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-    </>
-  )
-}
+  function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+      <main className='bg-black' >
+        <Navbar />
+        <Hero />
+        <Highlights />
+      </main>
+    )
+  }
 
 export default App
